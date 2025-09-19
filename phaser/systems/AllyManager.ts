9264299f63640a94f3564e806d4124440f201f4f
Miles Lilly <mileslilly2@gmail.com>
2025-09-19 21:47:10 +0000

    Fix ally formation offsets: center allies relative to player

--- phaser/systems/AllyManager.ts
+++ phaser/systems/AllyManager.ts
@@ -26,8 +26,12 @@ export default class AllyManager {
       const ally = this.scene.physics.add.sprite(this.player.x, this.player.y, 'player');
       ally.setDisplaySize(32, 32).setTint(0x00ffcc);
       this.allies.push({
-        sprite: ally,
-        offset: new Phaser.Math.Vector2(ship.x_norm ?? 0, ship.y_norm ?? 0),
+        sprite: ally,git 
+        offset: new Phaser.Math.Vector2(
+        (ship.x_norm - 50) / 100 * this.scene.scale.width,
+        (ship.y_norm - 50) / 100 * this.scene.scale.height
+      ),
+
         behavior: formation.behavior ?? 'follow',
       });
     });
